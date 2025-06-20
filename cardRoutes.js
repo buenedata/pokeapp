472f07865c5bf1e459e2292018447ad6837bd68a
buenedata <kristoffer@buenedata.no>
2025-06-20 20:45:42 +0200

    Delete cardRoutes.js

--- cardRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getCards, getCardById } = require('../controllers/cardController');
-
-router.get('/', getCards);
-router.get('/:id', getCardById);
-
-module.exports = router;
